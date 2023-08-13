1768e788724b3494ee97462b226e15080557c31d
nisarg <patelnisarg.3322@gmail.com>
2023-08-12 23:55:32 -0400

    added app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send("welcome to petharmony")
+})
+
+app.listen(3000 , () => {
+    console.log("Listening on 3000")
+})
