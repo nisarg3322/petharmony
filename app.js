40d345c0044dd00a3a340bb9cde27b239dedd927
Nisarg Patel <patelnisarg.3322@gmail.com>
2025-02-10 21:50:51 -0500

    fix: added vercel.json to run serverless

--- app.js
+++ app.js
@@ -1,79 +1,76 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const session = require('express-session');
-const mongoStore = require('connect-mongo')
-const flash = require('connect-flash');
-const ExpressError = require('./utils/ExpressError')
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const session = require("express-session");
+const mongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const serverless = require("serverless-http");
 
 // requiring models
-const User = require('./models/user_model')
-
+const User = require("./models/user_model");
 
 // Requiring routes
-const postRoutes = require('./routes/posts_routes');
-const userRoutes = require('./routes/user_routes');
+const postRoutes = require("./routes/posts_routes");
+const userRoutes = require("./routes/user_routes");
 
 // database mongoose connect code
-const dbUrl = process.env.DB_URL  || 'mongodb://127.0.0.1:27017/petharmony'; 
- 
-mongoose.connect(dbUrl)
-    .then(() => {
-        console.log("Mongo Connected!!!!")
-    })
-    .catch(err => {
-        console.log("On No error");
-        console.log(err);
-    })
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/petharmony";
 
-
-
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log("Mongo Connected!!!!");
+  })
+  .catch((err) => {
+    console.log("On No error");
+    console.log(err);
+  });
 
 // Ejs engine load
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // middleware to run every req object
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
 //Mongo store config
 const store = mongoStore.create({
-    mongoUrl: dbUrl,
-    touchAfter: 24 * 60 * 60,
-    crypto: {
-        secret: secret
-    }
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret: secret,
+  },
 });
 
 const sessionConfig = {
-    store,
-    secret: secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
-app.use(session(sessionConfig))
+  store,
+  secret: secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 app.use(flash());
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
@@ -81,40 +78,35 @@ passport.deserializeUser(User.deserializeUser());
 
 // locals middleware for every req object
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
-   
-
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 // Routers
-app.get('/',(req,res) => {
-    res.render('home.ejs')
-})
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
-app.use('/posts', postRoutes);
-app.use('/', userRoutes);
-
+app.use("/posts", postRoutes);
+app.use("/", userRoutes);
 
 // universal error for not visiting wrong URL
-app.all('*', (req, res, next) => {
-    // res.send('opps something went wrong')
-    
-    next(new ExpressError('Page Not Found', 404))
-})
+app.all("*", (req, res, next) => {
+  // res.send('opps something went wrong')
+
+  next(new ExpressError("Page Not Found", 404));
+});
 
 //universal error for general error
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('./error', { err })
-})
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(statusCode).render("./error", { err });
+});
 
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 // server listening code
-app.listen(port , () => {
-    console.log(`Listening on ${port}`)
-})
+module.exports = app;
+module.exports.handler = serverless(app);
